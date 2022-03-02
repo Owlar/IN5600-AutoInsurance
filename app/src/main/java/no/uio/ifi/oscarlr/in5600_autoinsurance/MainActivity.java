2dfdd6b4dc218322241de41a6c0399ad961d0513
Oscar Lund Ramstad <oscarlr@hiof.no>
2022-03-02 11:40:23 +0100

    Init

---
+++ app/src/main/java/no/uio/ifi/oscarlr/in5600_autoinsurance/MainActivity.java
@@ -0,0 +1,14 @@
+package no.uio.ifi.oscarlr.in5600_autoinsurance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
