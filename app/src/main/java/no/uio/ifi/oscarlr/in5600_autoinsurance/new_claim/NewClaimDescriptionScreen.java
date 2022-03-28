06b5881ede1b279d44d249dc589d01c3798ea518
Oscar Lund Ramstad <oscarlr@hiof.no>
2022-03-28 14:20:26 +0200

    Add get user location, add check for claim description

--- app/src/main/java/no/uio/ifi/oscarlr/in5600_autoinsurance/new_claim/NewClaimDescriptionScreen.java
+++ app/src/main/java/no/uio/ifi/oscarlr/in5600_autoinsurance/new_claim/NewClaimDescriptionScreen.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Objects;
 
@@ -34,7 +36,13 @@ public class NewClaimDescriptionScreen extends Fragment {
             viewPager.setCurrentItem(0);
         });
 
+        TextInputLayout description = view.findViewById(R.id.screenTwoClaimDescription);
         view.findViewById(R.id.nextButtonDescriptionScreen).setOnClickListener(view1 -> {
+            if (Objects.requireNonNull(description.getEditText()).getText().toString().isEmpty()) {
+                description.requestFocus();
+                Toast.makeText(requireContext(), "Please enter a claim description", Toast.LENGTH_SHORT).show();
+                return;
+            }
             viewPager.setCurrentItem(2);
             TextInputEditText editText = view.findViewById(R.id.descriptionEditText);
             newClaimSingleton.setClaimDes(Objects.requireNonNull(editText.getText()).toString());
--- app/src/main/java/no/uio/ifi/oscarlr/in5600_autoinsurance/new_claim/NewClaimLocationScreen.java
+++ app/src/main/java/no/uio/ifi/oscarlr/in5600_autoinsurance/new_claim/NewClaimLocationScreen.java
@@ -1,26 +1,50 @@
 package no.uio.ifi.oscarlr.in5600_autoinsurance.new_claim;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.List;
 
 import no.uio.ifi.oscarlr.in5600_autoinsurance.R;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
-public class NewClaimLocationScreen extends Fragment implements OnMapReadyCallback {
+public class NewClaimLocationScreen extends Fragment implements OnMapReadyCallback, EasyPermissions.PermissionCallbacks {
 
     private final ViewPager2 viewPager;
 
+    private final int PERMISSION_LOCATION_ID = 1;
+    private final String TAG = "NewClaimLocationScreen";
+
     private GoogleMap mMap = null;
+    private Button myLocationButton;
+    private FusedLocationProviderClient fusedLocationProviderClient = null;
+    private LatLng lastPosition;
 
     public NewClaimLocationScreen(ViewPager2 viewPager) {
         this.viewPager = viewPager;
@@ -33,6 +57,7 @@ public class NewClaimLocationScreen extends Fragment implements OnMapReadyCallba
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_new_claim_location_screen, container, false);
 
+        // TODO: Refactor view1 below and ID's in ViewPager2 XML files
         view.findViewById(R.id.backButtonLocationScreen).setOnClickListener(view1 -> viewPager.setCurrentItem(2));
 
         view.findViewById(R.id.nextButtonLocationScreen).setOnClickListener(view1 -> viewPager.setCurrentItem(4));
@@ -47,6 +72,9 @@ public class NewClaimLocationScreen extends Fragment implements OnMapReadyCallba
             e.printStackTrace();
         }
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        myLocationButton = view.findViewById(R.id.screenLocationMyPositionButton);
+
         mapView.getMapAsync(this);
 
         return view;
@@ -56,5 +84,83 @@ public class NewClaimLocationScreen extends Fragment implements OnMapReadyCallba
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
+        myLocationButton.setVisibility(View.VISIBLE);
+    }
+
+    @AfterPermissionGranted(PERMISSION_LOCATION_ID)
+    private void getUserLocation() {
+        if (EasyPermissions.hasPermissions(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+            myLocationButton.setOnClickListener(new View.OnClickListener() {
+                @SuppressLint("MissingPermission")
+                @Override
+                public void onClick(View v) {
+                    // Could use getCurrentLocation(), but getLastLocation() minimizes battery usage
+                    fusedLocationProviderClient.getLastLocation()
+                            .addOnSuccessListener(requireActivity(), new OnSuccessListener<Location>() {
+                                @Override
+                                public void onSuccess(Location location) {
+                                    if (location != null) {
+                                        lastPosition = new LatLng(location.getLatitude(), location.getLongitude());
+                                        Log.i(TAG, lastPosition.toString());
+
+                                        if (createMarker() != null) {
+                                            goToMarkedPosition();
+                                        }
+                                    }
+                                }
+                            });
+
+                }
+            });
+        } else {
+            EasyPermissions.requestPermissions(this, "Need location access to show MyLocation", PERMISSION_LOCATION_ID, Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private Marker createMarker() {
+        MarkerOptions markerOptions = new MarkerOptions();
+        // TODO: Potentially change title and snippet to those set in previous pages in ViewPager
+        markerOptions.title("My Position");
+        markerOptions.snippet("My Description");
+        markerOptions.position(lastPosition);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
+
+        return mMap.addMarker(markerOptions);
+    }
+
+    private void goToMarkedPosition() {
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastPosition, 13));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+
+        // TODO: Fix onResume loop on "Don't allow" permission
+
+        getUserLocation();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.i(TAG, "onViewCreated");
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        Log.i(TAG, "Permission granted");
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        Log.d(TAG, "Permission denied");
     }
 }
