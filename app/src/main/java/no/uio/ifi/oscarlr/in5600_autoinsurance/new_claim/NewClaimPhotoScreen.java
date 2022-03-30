5d806e7c6c9381409b6555b26b2f0f787a2e1057
sebimitch <sebimitch@gmail.com>
2022-03-30 23:09:07 +0200

    Changed claim cards. Added replace button. Started pick photo implementation.

--- app/src/main/java/no/uio/ifi/oscarlr/in5600_autoinsurance/new_claim/NewClaimPhotoScreen.java
+++ app/src/main/java/no/uio/ifi/oscarlr/in5600_autoinsurance/new_claim/NewClaimPhotoScreen.java
@@ -1,19 +1,32 @@
 package no.uio.ifi.oscarlr.in5600_autoinsurance.new_claim;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import no.uio.ifi.oscarlr.in5600_autoinsurance.R;
 
-
 public class NewClaimPhotoScreen extends Fragment {
 
     private final ViewPager2 viewPager;
+    private final ActivityResultLauncher<String> test = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri result) {
+
+                }
+            }
+    );
+
     public NewClaimPhotoScreen(ViewPager2 viewPager) {
         this.viewPager = viewPager;
     }
@@ -27,6 +40,11 @@ public class NewClaimPhotoScreen extends Fragment {
         view.findViewById(R.id.backButtonPhotoScreen).setOnClickListener(view1 -> viewPager.setCurrentItem(1));
 
         view.findViewById(R.id.nextButtonPhotoScreen).setOnClickListener(view1 -> viewPager.setCurrentItem(3));
+
+        view.findViewById(R.id.screenPhotoPickImageButton).setOnClickListener(view1 -> {
+            //  https://stackoverflow.com/a/63654043
+            test.launch("image/*");
+        });
 
         return view;
     }
